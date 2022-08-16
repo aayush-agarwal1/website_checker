8841d3862c09c952114d0d4eefe32cf91ff303bd
aayush.agarwal1 <aayush.agarwal1@swiggy.in>
2022-08-16 20:47:07 +0530

    Added API unit test

---
+++ pkg/api/api_test.go
@@ -0,0 +1,71 @@
+package api
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+const (
+	postResponse string = "Updated list of websites: www.google.com,www.facebook.com,www.fakewebsite1.com"
+)
+
+func TestPostWebsites(t *testing.T) {
+
+	requestBody, err := json.Marshal(postRequestBody{Websites: []string{"www.google.com", "www.facebook.com", "www.fakewebsite1.com"}})
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+	request := httptest.NewRequest(http.MethodPost, "/websites", bytes.NewReader(requestBody))
+	response := httptest.NewRecorder()
+	PostWebsites(response, request)
+	res := response.Result()
+	defer res.Body.Close()
+	data, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		t.Errorf("expected error to be nil got %v", err)
+	}
+	if string(data) != postResponse {
+		t.Errorf("expected `%s` got %v", postResponse, string(data))
+	}
+	if res.StatusCode != http.StatusOK {
+		t.Errorf("expected status code `%d` got %d", http.StatusOK, res.StatusCode)
+	}
+}
+
+func TestGetWebsites(t *testing.T) {
+
+	var tests = []struct {
+		queryParam     string
+		wantedResponse string
+	}{
+		{"", "{\"www.facebook.com\":\"INIT\",\"www.fakewebsite1.com\":\"INIT\",\"www.google.com\":\"INIT\"}"},
+		{"?name=www.google.com", "{\"www.google.com\":\"INIT\"}"},
+		{"?name=www.facebook.com,www.fakewebsite1.com", "{\"www.facebook.com\":\"INIT\",\"www.fakewebsite1.com\":\"INIT\"}"},
+	}
+
+	for i, tt := range tests {
+		testName := fmt.Sprintf("Test %d", i)
+		t.Run(testName, func(t *testing.T) {
+			request := httptest.NewRequest(http.MethodGet, "/websites"+tt.queryParam, nil)
+			response := httptest.NewRecorder()
+			GetWebsites(response, request)
+			res := response.Result()
+			defer res.Body.Close()
+			data, err := ioutil.ReadAll(res.Body)
+			if err != nil {
+				t.Errorf("expected error to be nil got %v", err)
+			}
+			if string(data) != tt.wantedResponse {
+				t.Errorf("expected `%s` got %v", tt.wantedResponse, string(data))
+			}
+			if res.StatusCode != http.StatusOK {
+				t.Errorf("expected status code `%d` got %d", http.StatusOK, res.StatusCode)
+			}
+		})
+	}
+}
