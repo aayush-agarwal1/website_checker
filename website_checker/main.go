89ab9601a1a3d51e1a20e255fcc2b2005813fd91
aayush.agarwal1 <aayush.agarwal1@swiggy.in>
2022-08-16 19:32:54 +0530

    Patched refactoring bug due to mismatch in keyword

--- website_checker/main.go
+++ website_checker/main.go
@@ -14,7 +14,7 @@ import (
 )
 
 func launchWebsiteCheckerCron(conf map[string]string) {
-	concurrency, _ := strconv.Atoi(conf["concurrency"])
+	concurrency, _ := strconv.Atoi(conf["cron.concurrency"])
 	delay, _ := strconv.Atoi(conf["cron.delaySeconds"])
 	go func() {
 		status_checker.ConcurrentStatusCheck(concurrency, delay)
