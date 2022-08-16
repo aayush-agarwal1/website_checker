5a7617d931e1e677ed9d706ec5c7c7dc5bd1a3d9
aayush.agarwal1 <aayush.agarwal1@swiggy.in>
2022-08-16 10:14:21 +0530

    Added default http server

--- website_checker/main.go
+++ website_checker/main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func basePath(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("got / request\n")
+	http.Error(w, "Invalid call, please check URI", http.StatusBadRequest)
+}
 
 func main() {
-	fmt.Println("Hello World")
+	http.HandleFunc("/", basePath)
+	err := http.ListenAndServe(":8080", nil)
+	if errors.Is(err, http.ErrServerClosed) {
+		fmt.Printf("Server closed\n")
+	} else if err != nil {
+		fmt.Printf("error starting server: %s\n", err)
+		os.Exit(1)
+	}
 }
