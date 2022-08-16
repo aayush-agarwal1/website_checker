68b434d17faf48aafb15a505dce49807054ef6b1
aayush.agarwal1 <aayush.agarwal1@swiggy.in>
2022-08-16 12:36:32 +0530

    Added websiteMap Model

---
+++ pkg/model/websiteMap.go
@@ -0,0 +1,42 @@
+package model
+
+type State string
+
+const (
+	UP   State = "UP"
+	DOWN       = "DOWN"
+	INIT       = "INIT"
+)
+
+type WebsiteProperties struct {
+	Status State
+}
+
+var websiteMapObject map[string]WebsiteProperties
+
+func init() {
+	websiteMapObject = newWebsiteMap()
+}
+
+func newWebsiteMap() map[string]WebsiteProperties {
+	return make(map[string]WebsiteProperties)
+}
+
+func InsertNewWebsite(website string) (err error, wasPresent bool) {
+	err = nil
+	_, wasPresent = websiteMapObject[website]
+	if !wasPresent {
+		websiteMapObject[website] = WebsiteProperties{
+			Status: INIT,
+		}
+	}
+	return
+}
+
+func GetWebsiteList() (websites []string) {
+	websites = make([]string, len(websiteMapObject))
+	for website := range websiteMapObject {
+		websites = append(websites, website)
+	}
+	return
+}
